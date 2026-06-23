9820dc8326e42dfea75fd83508eb1905de45dc99
pedroaugustog <pedroaugusto172015@gmail.com>
2026-06-22 22:22:16 -0300

    Commit Inicial

---
+++ src/ControleTerrenos.java
@@ -0,0 +1,14 @@
+public class ControleTerrenos {
+
+    public static double CalcularArea(
+            double comp,
+            double larg
+    )
+    {
+        return comp * larg;
+    }
+
+}
+
+
+
---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o comprimento do terreno: ");
+        double comp = scanner.nextDouble();
+
+        System.out.print("Digite o largura do terreno: ");
+        double larg = scanner.nextDouble();
+
+        double area = ControleTerrenos.CalcularArea(comp, larg);
+
+        System.out.print("O terreno com " + comp + " m de comprimento e " + larg +
+                " m de largura, possui uma área de " + area + " m².");
+    }
+
+
+
+
+
+}
